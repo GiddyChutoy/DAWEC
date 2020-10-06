bdf8e79880bb515e7d2e16718697315a4829decd
Adrian Mateos <adrian@adrian>
2020-10-06 09:56:26 +0200

    [MOD] Ejercicio 6 completo

--- Ejercicios/manipulacion.js
+++ Ejercicios/manipulacion.js
@@ -13,3 +13,21 @@ let empiezanPorA = arrayPalabras.filter(palabra => palabra[0] == "a");
 
 //Ejercicio 6
 
+var notas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3.5, 4.5, 7.5, 5.6, 9.5];
+console.log(notas.find(numero => numero == 5.5));
+//devuelve undefined porque no existe el numero indicado en el array
+
+console.log(notas.every(numero => numero >= 5)); //Devuelve false porque comprueba si todos son aprobados
+console.log(notas.some(numero => numero >= 5));//Devuelve true porque si hay al menos un aprobado.
+
+var palabras = ["palra1", "pra2", "palabra3", "palabra4", "pa5", "palabra6", "palabra7", "palabra8", "labra9", "pal10", "pabra11", "palabra12"];
+var palabras6 = palabras.filter(palabra => palabra.length == 6);
+console.log(palabras6);
+//devuelve las palabras que tengan una longitud de 6
+
+var telefonos = [628457812, 852417963, 789456123, 159753864, 842963852, 715894613, 56894231, 462486139];
+console.log(telefonos.some(numero => {
+    numerostelf = numero.toString();
+    return numerostelf[numerostelf.length - 1] == "3";
+}));
+//devuelve los numeros de telefono que terminen en 3
